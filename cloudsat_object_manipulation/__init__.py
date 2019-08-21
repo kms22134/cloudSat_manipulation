5a26fc918a8633dda63cc18b0b0f377fe76287b4
Kevin <ksmalley@tamu.edu>
2019-08-21 10:50:48 -0500

    updated version number from 0.1.3 to 0.1.4

--- cloudsat_object_manipulation/__init__.py
+++ cloudsat_object_manipulation/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Kevin Smalley"""
 __email__ = 'ksmalley@tamu.edu'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/kms22134/cloudsat_manipulation',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
     cmdclass = {'build_ext': build_ext}
 )
